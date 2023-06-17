fefd9ff7167e4f4d163424c45c1da7ebabd2cb13
Lewis Bray <32199493+LewisBray@users.noreply.github.com>
2023-06-17 12:03:43 +0100

    Implemented diffuse spheres
    Also added gamma correction

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@
 
 using u32 = unsigned int;
 
-u32 noise_1d(const int x, const u32 seed) {
+using real = double;
+static constexpr real REAL_MAX = DBL_MAX;
+
+static u32 noise_1d(const int x, const u32 seed) {
     static constexpr u32 BIT_NOISE_1 = 0x68E31DA4;
     static constexpr u32 BIT_NOISE_2 = 0xB5297A4D;
     static constexpr u32 BIT_NOISE_3 = 0x1B56C4E9;
@@ -21,7 +24,7 @@ u32 noise_1d(const int x, const u32 seed) {
     return mangled;
 }
 
-u32 noise_3d(const int x, const int y, const int z, const u32 seed) {
+static u32 noise_3d(const int x, const int y, const int z, const u32 seed) {
     static constexpr int PRIME_1 = 198491317;
     static constexpr int PRIME_2 = 6542989;
     const int new_x = x + PRIME_1 * y + PRIME_2 * z;
@@ -29,39 +32,57 @@ u32 noise_3d(const int x, const int y, const int z, const u32 seed) {
     return result;
 }
 
-float float_from_rng_seed(const u32 rng_seed) {
-    return static_cast<float>(rng_seed) / static_cast<float>(UINT_MAX);
+static real real_from_rng_seed(const u32 rng_seed) {
+    return static_cast<real>(rng_seed) / static_cast<real>(UINT_MAX);
 }
 
 struct Vec3 {
-    float x;
-    float y;
-    float z;
+    real x;
+    real y;
+    real z;
 };
 
-Vec3 operator+(const Vec3& lhs, const Vec3& rhs) {
+static Vec3 operator+(const Vec3& lhs, const Vec3& rhs) {
     return Vec3{lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z};
 }
 
-Vec3 operator-(const Vec3& lhs, const Vec3& rhs) {
+static Vec3 operator-(const Vec3& lhs, const Vec3& rhs) {
     return Vec3{lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z};
 }
 
-Vec3 operator*(const float scalar, const Vec3& v) {
+static Vec3 operator*(const real scalar, const Vec3& v) {
     return Vec3{scalar * v.x, scalar * v.y, scalar * v.z};
 }
 
-float operator*(const Vec3& lhs, const Vec3& rhs) {
+static real operator*(const Vec3& lhs, const Vec3& rhs) {
     return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z;
 }
 
-float magnitude(const Vec3& v) {
+static real magnitude(const Vec3& v) {
     return std::sqrt(v * v);
 }
 
-Vec3 normalise(const Vec3& v) {
-    const float inverse_magnitude = 1.0f / magnitude(v);
+static Vec3 normalise(const Vec3& v) {
+    const real inverse_magnitude = 1.0f / magnitude(v);
     return inverse_magnitude * v;
+}
+
+static constexpr u32 RNG_SEED = 0;
+
+static Vec3 random_unit_vector(const Vec3 seed) {
+    const int x = static_cast<int>(1000000000.0f * seed.x);
+    const int y = static_cast<int>(1000000000.0f * seed.y);
+    const int z = static_cast<int>(1000000000.0f * seed.z);
+
+    const u32 x_noise = noise_1d(x, RNG_SEED);
+    const u32 y_noise = noise_1d(y, RNG_SEED);
+    const u32 z_noise = noise_1d(z, RNG_SEED);
+
+    const real random_x = real_from_rng_seed(x_noise) - 0.5f;
+    const real random_y = real_from_rng_seed(y_noise) - 0.5f;
+    const real random_z = real_from_rng_seed(z_noise) - 0.5f;
+    const Vec3 random_vector = Vec3{random_x, random_y, random_z};
+    return normalise(random_vector);
 }
 
 struct Ray {
@@ -71,7 +92,7 @@ struct Ray {
 
 struct Sphere {
     Vec3 centre;
-    float radius;
+    real radius;
 };
 
 template <typename T>
@@ -81,8 +102,8 @@ struct Maybe {
 };
 
 struct SphereIntersections {
-    float distance_1;
-    float distance_2;
+    real distance_1;
+    real distance_2;
 };
 
 static Maybe<SphereIntersections> intersect(const Ray& ray, const Sphere& sphere) noexcept {
@@ -91,18 +112,18 @@ static Maybe<SphereIntersections> intersect(const Ray& ray, const Sphere& sphere
     Maybe<SphereIntersections> result = {};
 
     const Vec3 ray_origin_to_sphere_centre = sphere.centre - ray.origin;
-    const float intersections_mid_point_distance = ray_origin_to_sphere_centre * ray.direction;
+    const real intersections_mid_point_distance = ray_origin_to_sphere_centre * ray.direction;
 
-    const float ray_start_to_sphere_centre_distance_squared = ray_origin_to_sphere_centre * ray_origin_to_sphere_centre;
-    const float intersections_mid_point_distance_squared = intersections_mid_point_distance * intersections_mid_point_distance;
-    const float sphere_centre_to_intersections_mid_point_squared = ray_start_to_sphere_centre_distance_squared - intersections_mid_point_distance_squared;
-    const float sphere_radius_squared = sphere.radius * sphere.radius;
-    const float intersections_mid_point_to_intersections_distance_squared = sphere_radius_squared - sphere_centre_to_intersections_mid_point_squared;
+    const real ray_start_to_sphere_centre_distance_squared = ray_origin_to_sphere_centre * ray_origin_to_sphere_centre;
+    const real intersections_mid_point_distance_squared = intersections_mid_point_distance * intersections_mid_point_distance;
+    const real sphere_centre_to_intersections_mid_point_squared = ray_start_to_sphere_centre_distance_squared - intersections_mid_point_distance_squared;
+    const real sphere_radius_squared = sphere.radius * sphere.radius;
+    const real intersections_mid_point_to_intersections_distance_squared = sphere_radius_squared - sphere_centre_to_intersections_mid_point_squared;
     if (intersections_mid_point_to_intersections_distance_squared < 0.0f) {
         return result;
     }
 
-    const float intersections_mid_point_to_intersections_distance = std::sqrt(intersections_mid_point_to_intersections_distance_squared);
+    const real intersections_mid_point_to_intersections_distance = std::sqrt(intersections_mid_point_to_intersections_distance_squared);
     result.value.distance_1 = intersections_mid_point_distance - intersections_mid_point_to_intersections_distance;
     result.value.distance_2 = intersections_mid_point_distance + intersections_mid_point_to_intersections_distance;
     result.valid = true;
@@ -110,21 +131,49 @@ static Maybe<SphereIntersections> intersect(const Ray& ray, const Sphere& sphere
     return result;
 }
 
-struct Colour {
-    float r;
-    float g;
-    float b;
+struct ClosestSphereIntersection {
+    int index;
+    real distance;
 };
 
-Colour operator+(const Colour& lhs, const Colour& rhs) {
+static Maybe<ClosestSphereIntersection> intersect(const Ray& ray, const Sphere* const spheres, const int count) {
+    Maybe<ClosestSphereIntersection> closest_intersection = {};
+    closest_intersection.valid = false;
+    closest_intersection.value.index = -1;
+    closest_intersection.value.distance = REAL_MAX;
+    for (int sphere_index = 0; sphere_index < count; ++sphere_index) {
+        const Maybe<SphereIntersections> intersection_result = intersect(ray, spheres[sphere_index]);
+        if (intersection_result.valid) {
+            if (0.0f < intersection_result.value.distance_1 && intersection_result.value.distance_1 < closest_intersection.value.distance) {
+                closest_intersection.valid = true;
+                closest_intersection.value.index = sphere_index;
+                closest_intersection.value.distance = intersection_result.value.distance_1;
+            } else if (0.0f < intersection_result.value.distance_2 && intersection_result.value.distance_2 < closest_intersection.value.distance) {
+                closest_intersection.valid = true;
+                closest_intersection.value.index = sphere_index;
+                closest_intersection.value.distance = intersection_result.value.distance_2;
+            }
+        }
+    }
+
+    return closest_intersection;
+}
+
+struct Colour {
+    real r;
+    real g;
+    real b;
+};
+
+static Colour operator+(const Colour& lhs, const Colour& rhs) {
     return Colour{lhs.r + rhs.r, lhs.g + rhs.g, lhs.b + rhs.b};
 }
 
-Colour operator*(const float scalar, const Colour& colour) {
+static Colour operator*(const real scalar, const Colour& colour) {
     return Colour{scalar * colour.r, scalar * colour.g, scalar * colour.b};
 }
 
-Colour& operator+=(Colour& lhs, const Colour& rhs) {
+static Colour& operator+=(Colour& lhs, const Colour& rhs) {
     lhs.r += rhs.r;
     lhs.g += rhs.g;
     lhs.b += rhs.b;
@@ -132,12 +181,47 @@ Colour& operator+=(Colour& lhs, const Colour& rhs) {
     return lhs;
 }
 
-Colour& operator/=(Colour& lhs, const float scalar) {
+static Colour& operator/=(Colour& lhs, const real scalar) {
     lhs.r /= scalar;
     lhs.g /= scalar;
     lhs.b /= scalar;
 
     return lhs;
+}
+
+static Colour background_gradient(const Ray& ray) {
+    const real t = 0.5f * (ray.direction.y + 1.0f);
+    return (1.0f - t) * Colour{1.0, 1.0, 1.0} + t * Colour{0.5, 0.7, 1.0};
+}
+
+struct Scene {
+    const Sphere* spheres;
+    int sphere_count;
+};
+
+static Colour intersect(Ray ray, const Scene& scene) {
+    static constexpr int MAX_BOUNCE_COUNT = 50;
+
+    Colour colour = {};
+    real strength = 1.0f;
+    for (int bounce_index = 0; bounce_index < MAX_BOUNCE_COUNT; ++bounce_index) {
+        const Maybe<ClosestSphereIntersection> closest_sphere_intersection = intersect(ray, scene.spheres, scene.sphere_count);
+        if (closest_sphere_intersection.valid) {
+            const Sphere& sphere = scene.spheres[closest_sphere_intersection.value.index];
+            const Vec3 intersection_point = ray.origin + closest_sphere_intersection.value.distance * ray.direction;
+            const Vec3 sphere_unit_normal = normalise(intersection_point - sphere.centre);
+            ray.origin = intersection_point + 0.001 * sphere_unit_normal;
+            const Vec3 random = random_unit_vector(ray.origin);
+            ray.direction = normalise(sphere_unit_normal + random);
+
+            strength *= 0.5f;
+        } else {
+            colour = strength * background_gradient(ray);
+            break;
+        }
+    }
+
+    return colour;
 }
 
 #include <Windows.h>
@@ -155,11 +239,6 @@ LRESULT CALLBACK main_window_proc(const HWND window, const UINT message, const W
             return DefWindowProcW(window, message, w_param, l_param);
         }
     }
-}
-
-Colour background_gradient(const Ray& ray) {
-    const float t = 0.5f * (ray.direction.y + 1.0f);
-    return (1.0f - t) * Colour{1.0, 1.0, 1.0} + t * Colour{0.5, 0.7, 1.0};
 }
 
 int WINAPI wWinMain(const HINSTANCE instance, HINSTANCE, PWSTR, int) {
@@ -182,9 +261,9 @@ int WINAPI wWinMain(const HINSTANCE instance, HINSTANCE, PWSTR, int) {
     const ATOM atom = RegisterClassW(&window_class);
     assert(atom != 0);
 
-    static constexpr float ASPECT_RATIO = 16.0f / 9.0f;
+    static constexpr real ASPECT_RATIO = 16.0f / 9.0f;
     static constexpr int CLIENT_WIDTH = 400;
-    static constexpr int CLIENT_HEIGHT = static_cast<int>(static_cast<float>(CLIENT_WIDTH) / ASPECT_RATIO);
+    static constexpr int CLIENT_HEIGHT = static_cast<int>(static_cast<real>(CLIENT_WIDTH) / ASPECT_RATIO);
 
     static constexpr DWORD WINDOW_STYLE = WS_CAPTION | WS_SYSMENU;
 
@@ -236,9 +315,9 @@ int WINAPI wWinMain(const HINSTANCE instance, HINSTANCE, PWSTR, int) {
     unsigned char* pixels = static_cast<unsigned char*>(VirtualAlloc(0, 4 * CLIENT_WIDTH * CLIENT_HEIGHT, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE));
     assert(pixels != nullptr);
 
-    static constexpr float VIEWPORT_HEIGHT = 2.0f;
-    static constexpr float VIEWPORT_WIDTH = ASPECT_RATIO * VIEWPORT_HEIGHT;
-    static constexpr float FOCAL_LENGTH = 1.0f;
+    static constexpr real VIEWPORT_HEIGHT = 2.0f;
+    static constexpr real VIEWPORT_WIDTH = ASPECT_RATIO * VIEWPORT_HEIGHT;
+    static constexpr real FOCAL_LENGTH = 1.0f;
 
     static constexpr Vec3 BOTTOM_LEFT{-0.5f * VIEWPORT_WIDTH, -0.5f * VIEWPORT_HEIGHT, -FOCAL_LENGTH};
 
@@ -248,52 +327,35 @@ int WINAPI wWinMain(const HINSTANCE instance, HINSTANCE, PWSTR, int) {
         Sphere{Vec3{0.0f, -100.5f, -1.0f}, 100.0f}
     };
 
-    static constexpr int SAMPLES_PER_PIXEL = 100;
+    const Scene scene{SPHERES, SPHERE_COUNT};
 
-    static constexpr u32 RNG_SEED = 0;
+    static constexpr int SAMPLES_PER_PIXEL = 100;
 
     for (int row = 0; row < CLIENT_HEIGHT; ++row) {
         for (int column = 0; column < CLIENT_WIDTH; ++column) {
             Colour colour = {};
             for (int sample_index = 0; sample_index < SAMPLES_PER_PIXEL; ++sample_index) {
                 const u32 u_rng_seed = noise_3d(row, column, sample_index + 0, RNG_SEED);
-                const float u_randomness = float_from_rng_seed(u_rng_seed);
-                const float u = (static_cast<float>(column) + u_randomness) / static_cast<float>(CLIENT_WIDTH - 1);
+                const real u_randomness = real_from_rng_seed(u_rng_seed);
+                const real u = (static_cast<real>(column) + u_randomness) / static_cast<real>(CLIENT_WIDTH - 1);
 
                 const u32 v_rng_seed = noise_3d(row, column, sample_index + 1, RNG_SEED);
-                const float v_randomness = float_from_rng_seed(v_rng_seed);
-                const float v = (static_cast<float>(row) + v_randomness) / static_cast<float>(CLIENT_HEIGHT - 1);
+                const real v_randomness = real_from_rng_seed(v_rng_seed);
+                const real v = (static_cast<real>(row) + v_randomness) / static_cast<real>(CLIENT_HEIGHT - 1);
 
                 const Vec3 ray_origin{0.0f, 0.0f, 0.0f};
                 const Vec3 ray_direction = normalise(Vec3{BOTTOM_LEFT + u * Vec3{VIEWPORT_WIDTH, 0.0f, 0.0f} + v * Vec3{0.0f, VIEWPORT_HEIGHT, 0.0f}});
                 const Ray ray{ray_origin, ray_direction};
 
-                int closest_intersecting_sphere_index = -1;
-                float closest_intersecting_sphere_distance = FLT_MAX;
-                for (int sphere_index = 0; sphere_index < SPHERE_COUNT; ++sphere_index) {
-                    const Maybe<SphereIntersections> intersection_result = intersect(ray, SPHERES[sphere_index]);
-                    if (intersection_result.valid) {
-                        if (0.0f < intersection_result.value.distance_1 && intersection_result.value.distance_1 < closest_intersecting_sphere_distance) {
-                            closest_intersecting_sphere_index = sphere_index;
-                            closest_intersecting_sphere_distance = intersection_result.value.distance_1;
-                        } else if (0.0f < intersection_result.value.distance_2 && intersection_result.value.distance_2 < closest_intersecting_sphere_distance) {
-                            closest_intersecting_sphere_index = sphere_index;
-                            closest_intersecting_sphere_distance = intersection_result.value.distance_2;
-                        }
-                    }
-                }
-
-                if (closest_intersecting_sphere_index != -1) {
-                    const Sphere& sphere = SPHERES[closest_intersecting_sphere_index];
-                    const Vec3 intersection_point = ray.origin + closest_intersecting_sphere_distance * ray.direction;
-                    const Vec3 sphere_unit_normal = normalise(intersection_point - sphere.centre);
-                    colour += Colour{0.5f * (sphere_unit_normal.x + 1.0f), 0.5f * (sphere_unit_normal.y + 1.0f), 0.5f * (sphere_unit_normal.z + 1.0f)};
-                } else {
-                    colour += background_gradient(ray);
-                }
+                colour += intersect(ray, scene);
             }
 
-            colour /= static_cast<float>(SAMPLES_PER_PIXEL);
+            colour /= static_cast<real>(SAMPLES_PER_PIXEL);
+
+            // gamma correct
+            colour.r = std::sqrt(colour.r);
+            colour.g = std::sqrt(colour.g);
+            colour.b = std::sqrt(colour.b);
 
             const int index = 4 * (row * CLIENT_WIDTH + column);
             pixels[index + 0] = static_cast<unsigned char>(255.0f * colour.b);
